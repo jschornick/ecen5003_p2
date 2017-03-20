59ce9e56fc4d6959d560deeddec4e206a8c7d08f
Jeff Schornick <jesc5667@colorado.edu>
2017-03-19 19:06:39 -0600

    [M2] Successfully reading I2C temperature sensor

--- m2/lab_2.11/I2C/src/main.cpp
+++ m2/lab_2.11/I2C/src/main.cpp
@@ -7,9 +7,8 @@ SERIAL COMMUNICATION
 	
 	Input: temperature sensor
 	Output: PC
-	
-	GOOD LUCK!
  *----------------------------------------------------------------------------*/
+
 #include "mbed.h"
 #include "pindef.h"
 
@@ -20,6 +19,7 @@ Serial pc(UART_TX, UART_RX);
 //I2C address of temperature sensor DS1631
 const int temp_addr = 0x90;
 
+enum cmds { START_CONVERT, READ_TEMP };
 char cmd[] = {0x51, 0xAA};
 char read_temp[2];
 
@@ -28,6 +28,8 @@ char read_temp[2];
  *----------------------------------------------------------------------------*/
 
 int main(){
+
+  pc.printf("Hello\n");  
 	while(1){
 		/*
 		Write the Start Convert T command to the sensor
@@ -35,18 +37,14 @@ int main(){
 		Read the 16-bit temperature data
 		*/
 		
-		//Write your code here
-		
+		temp_sensor.write( temp_addr, &cmd[START_CONVERT], 1 );
+    temp_sensor.write( temp_addr, &cmd[READ_TEMP], 1 );
+    temp_sensor.read( temp_addr, read_temp, 2 );		
 		
 		//Convert temperature to Celsius
 		float temp = (float((read_temp[0] << 8) | read_temp[1]) / 256);
 		
-		//Print temperature to the serial monitor
-		
-		//Write your code here
-		
+    pc.printf("Temp: %0.2f degrees C\n", temp);
 		
 	}
 }
-
-// *******************************ARM University Program Copyright (c) ARM Ltd 2014*************************************
